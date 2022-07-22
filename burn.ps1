b4574b652c08d1468cf60879fbfa4c4edbc3724a
lacsaP <patatetom@users.noreply.github.com>
2022-07-22 13:08:33 +0200

    regular expression
    linux (sha1  filename) & windows (sha1 *filename)

--- burn.ps1
+++ burn.ps1
@@ -1589,8 +1589,8 @@ If (Test-Path -PathType Leaf "$Image") {
     Write-Host 'Calculating the SHA1 footprint...'
     $Sha1 = Get-FileHash -Algorithm Sha1 "$Image"
     If (Test-Path -PathType Leaf "$Image.sha1") {
-        $Rexp = [Regex]::Escape((Get-Item "$Image").Name)
-        $Ctrl = Select-String -Pattern "$Rexp$" "$Image.sha1"
+        $Rexp = "^[0-9a-fA-F]{40}\s+\*?$([Regex]::Escape((Get-Item "$Image").Name))$"
+        $Ctrl = Select-String -Pattern $Rexp "$Image.sha1"
         If ( $Sha1.Hash.ToLower() -Eq $Ctrl.Line.SubString(0,40).ToLower() ) {
             Write-Host -ForegroundColor Green $Sha1.Hash
         } Else {
